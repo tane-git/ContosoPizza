d50f7a22487326b09c5512cf23ed4e74d3374f6e
tane-git <tane.alexander.wilson@gmail.com>
2022-11-23 19:48:10 +1300

    extending contosoPizza tutorial

---
+++ Models/Drinks.cs
@@ -0,0 +1,8 @@
+namespace ContosoPizza.Models;
+
+public class Drinks
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsSugarFree { get; set; }
+}
---
+++ Models/Pasta.cs
@@ -0,0 +1,8 @@
+namespace ContosoPizza.Models;
+
+public class Pasta
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsDairyFree { get; set; }
+}
